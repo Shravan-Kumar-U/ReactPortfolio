4e58623fd70b902e48ddbf42911d57fec00679ee
Shravan-Kumar-U <kumarshravan12546@gmail.com>
2025-06-02 14:31:03 +0530

    changed the Home page

--- simple_Portfolio/src/Components/Home/Home.jsx
+++ simple_Portfolio/src/Components/Home/Home.jsx
@@ -31,18 +31,6 @@ const Home = () => {
       <div className='w-full md:w-1/2 flex justify-center md:justify-end mt-10 md:mt-0'>
         <img className='w-2/4 md:w-2/3 max-w-sm' src={avatarImg} alt="Avatar" />
       </div>
-
-      
-{/*     
-        <div className='text-white flex w-full justify-between items-start p-10 md:p-20'>
-         <div className='md:w-2/4 md:pt-10'>
-             <h1 className='text-3l md:text-6xl font-bold flex leading-normal tracking-tighter'>Hello I'am Shravan</h1>
-             <p className='text-sm md:text-2xl tracking-tight'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique atque, nam reprehenderit cum eveniet veritatis accusamus voluptates, .</p>
-             <button className='mt-5 md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85 duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]'>Contact me</button>
-        </div>
-         <div><img className='w-2/5' src={avatarImg} alt="" /></div>
-     </div> */}
-
       
       
     </div>
